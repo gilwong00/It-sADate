5a6b901da5a615eae46874531fe14e7413b4f9cc
Gilbert Wong <gilwong3105@gmail.com>
2017-09-29 03:27:24 -0700

    refactored App.js to be a pure component and removed login.js file

--- public/src/components/app.js
+++ public/src/components/app.js
@@ -1,52 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 //importing Components
 import Login from '../containers/login';
 import Header from '../containers/header';
 import Welcome from './Welcome/Welcome';
 
-class App extends Component {
-    // componentDidMount() {
-    //     console.log(this.props.signIn);
-    // }
-    // shouldComponentUpdate(nextProps) {
-    //     console.log(nextProps)
-    //     if (nextProps.signIn !== this.props.signIn) {
-    //         return true;
-    //     }
-    //     else {
-    //         return false;
-    //     }
-    // }
-    // componentDidUpdate(newProps, nextState) {
-    //     console.log(this.props.signIn);
-    //     let { signIn, signUp } = this.props.signIn;
+const App = () => {
+    return(
+        <BrowserRouter>
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Welcome} />
+                    <Route path="/App/:key" component={Login}/>
+                
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
+};
 
-    //     if(signIn) {
-    //         this.props.history
-    //     }
-    // }
-    render() {
-        return(
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Switch>
-                        <Route exact path="/" component={Welcome} />
-                        <Route path="/App/:key" component={Login}/>
-                    
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        )
-    }
-}
-
-function mapStateToProps(state, ownProps) {
-    return {signIn: state.registeration}
-}
-
-export default connect(mapStateToProps)(App);
-
+export default App;
